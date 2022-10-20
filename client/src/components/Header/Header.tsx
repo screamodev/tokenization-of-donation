6ef81c522fc23342e8ffc4388f9d59edfc04188a
Arsenii Yurchenko <yurchenko-a@lxuser-ThinkPad-E14-Gen-2>
2022-10-20 09:59:35 +0300

    Fix styles. Add connect wallet logic

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,28 +1,43 @@
 import { FC } from 'react';
+import useEth from '../../contexts/EthContext/useEth';
 import './header.scss';
 
-export const Header: FC = () => (
-  <div className="header">
-    <div className="header-logo">
-      <div className="header-logo-icon">TOD</div>
-      <p className="header-logo-text">
-        Tokenization
-        <br />
-        Of
-        <br />
-        Donation
-      </p>
-    </div>
+export const Header: FC = () => {
+  const { state, connectWallet } = useEth();
 
-    <nav className="header-navbar">
-      <div className="header-navbar-item">Home</div>
-      <div className="header-navbar-item">Create Campaign</div>
-      <div className="header-navbar-item">About</div>
-    </nav>
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <div className="header-logo-icon">TOD</div>
+        <p className="header-logo-text">
+          Tokenization
+          <br />
+          Of
+          <br />
+          Donation
+        </p>
+      </div>
 
-    <div className="header-authorization">
-      <button className="header-authorization-sign-up button" type="submit">Sign up</button>
-      <button className="header-authorization-sign-in button" type="submit">Sign in</button>
+      <nav className="header-navbar">
+        <div className="header-navbar-item">Home</div>
+        <div className="header-navbar-item">Create Campaign</div>
+        <div className="header-navbar-item">About</div>
+      </nav>
+
+      <div className="header-authorization">
+        {state.isMetamaskInstalled
+          ? state.userAccount
+            ? (
+              <p className="header-authorization-account-address">{state.userAccount}</p>
+            )
+            : (<button onClick={connectWallet} className="header-authorization-sign-up button" type="submit">Connect wallet</button>)
+          : (
+            <a href="https://metamask.io/download/" target="_blank" rel="noreferrer">
+              <button type="button">Install metamask wallet to login</button>
+            </a>
+          )}
+
+      </div>
     </div>
-  </div>
-);
+  );
+};
