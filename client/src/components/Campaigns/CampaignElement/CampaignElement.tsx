6ddb5991daa2b92e9d320262f57d0d53c8085637
Arsenii Yurchenko <yurchenko-a@lxuser-ThinkPad-E14-Gen-2>
2022-10-24 10:47:13 +0300

    Add donate frontend logic

--- client/src/components/Campaigns/CampaignElement/CampaignElement.tsx
+++ client/src/components/Campaigns/CampaignElement/CampaignElement.tsx
@@ -1,36 +1,105 @@
-import { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { ProgressBar } from '../../ProgressBar/ProgressBar';
 import { convertSecondsToDays } from '../../../utils/utils';
+import useEth from '../../../contexts/EthContext/useEth';
+import { actions } from '../../../contexts/EthContext';
 import './campaignElement.scss';
 
 interface CampaignElementProps {
+    id: number;
     title: string;
     description: string;
     goal: number;
     alreadyDonated: number
     endsAt: number;
+    donate: () => any;
 }
 
 export const CampaignElement: FC<CampaignElementProps> = ({
-  title, description, goal, alreadyDonated, endsAt,
-}) => (
-  <div className="campaigns-element-item">
-    <div className="campaigns-element-item-image">some image</div>
-    <div className="campaigns-element-item-content">
-      <div className="campaigns-element-item-content-text">
-        <h2 className="campaigns-element-item-content-text-title">{title}</h2>
-        <p className="campaigns-element-item-content-text-description">{description}</p>
-      </div>
-      <div className="campaigns-element-item-content-details">
-        <div className="campaigns-element-item-content-details-goal">
-          <h3 className="font-bold">{`Goal is: ${goal} ETH`}</h3>
-          <ProgressBar
-            goal={goal}
-            alreadyDonated={+alreadyDonated}
-          />
+  id,
+  title,
+  description,
+  goal,
+  alreadyDonated,
+  endsAt,
+  donate,
+}) => {
+  const {
+    state: {
+      web3,
+      userAccount,
+    }, dispatch,
+  } = useEth();
+
+  const [donationAmount, setDonationAmount] = useState('');
+  const [isDonateClicked, setIsDonateClicked] = useState(false);
+
+  const handleChangeDonate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDonationAmount(e.target.value);
+  };
+
+  const handleClickDonate = () => {
+    setIsDonateClicked(true);
+  };
+
+  const handleSendDonation = async (value: number) => {
+    await donate().send({ from: userAccount, value: web3.utils.toWei(`${value}`, 'ether') })
+      .then(({ events: { donated: { returnValues } } }: any) => {
+        const donatedAmount = +web3.utils.fromWei(returnValues[0], 'ether');
+        const fixedAmount = +donatedAmount.toFixed(3);
+
+        dispatch({
+          type: actions.donateFunds,
+          data: { id, donatedAmount: fixedAmount },
+        });
+      });
+  };
+
+  return (
+    <div className="campaigns-element-item">
+      <div className="campaigns-element-item-image">some image</div>
+      <div className="campaigns-element-item-content">
+        <div className="campaigns-element-item-content-text">
+          <h2 className="campaigns-element-item-content-text-title">{title}</h2>
+          <p className="campaigns-element-item-content-text-description">{description}</p>
         </div>
-        <p className="campaigns-element-item-content-details-endsAt">{`${convertSecondsToDays(+endsAt)} days left`}</p>
+        <div className="campaigns-element-item-content-details">
+          <div className="campaigns-element-item-content-details-goal">
+            <h3 className="font-bold">{`Goal is: ${goal} ETH`}</h3>
+            <ProgressBar
+              goal={goal}
+              alreadyDonated={+alreadyDonated.toFixed(3)}
+            />
+          </div>
+          <p className="campaigns-element-item-content-details-endsAt">{`${convertSecondsToDays(+endsAt)} days left`}</p>
+        </div>
       </div>
+      {!isDonateClicked ? (
+        <button
+          className="campaigns-element-item-donate-button"
+          onClick={handleClickDonate}
+          type="button"
+        >
+          Donate
+        </button>
+      ) : (
+        <div className="campaigns-element-item-donate-funds">
+          <input
+            placeholder="Enter ETH amount"
+            className="campaigns-element-item-donate-funds-input"
+            type="number"
+            onChange={handleChangeDonate}
+            value={donationAmount}
+          />
+          <button
+            onClick={() => handleSendDonation(+donationAmount)}
+            className="campaigns-element-item-donate-funds-button"
+            type="button"
+          >
+            Send
+          </button>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
