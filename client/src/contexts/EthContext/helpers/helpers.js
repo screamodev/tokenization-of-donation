ebace19c066e1e22555194c1f7b6dc0b7d8f68ed
Arsenii Yurchenko <yurchenko-a@lxuser-ThinkPad-E14-Gen-2>
2022-11-08 20:52:50 +0200

    Create add nft logic

--- client/src/contexts/EthContext/helpers/helpers.js
+++ client/src/contexts/EthContext/helpers/helpers.js
@@ -10,6 +10,22 @@ export const getCampaignsAddresses = async (contractInstance, campaignsCount) =>
   return campaignsAddressesPromisses;
 };
 
+export const getNftsAddresses = async (contractInstance, userAccount, userNftCount) => {
+  const userNftsAddressesPromises = [];
+
+  for (let i = 1; i <= +userNftCount; i++) {
+    const userNftAddress = contractInstance
+      .methods.donatersNfts(userAccount, i).call();
+    userNftsAddressesPromises.push(userNftAddress);
+  }
+
+  const resolvedNftsAdresses = await Promise.all(userNftsAddressesPromises);
+
+  console.log(resolvedNftsAdresses);
+
+  return resolvedNftsAdresses;
+};
+
 export const filterCampaignInstance = async (web3, abi, contractAddress, userAccount) => {
   const { methods: campaignMethods } = new web3
     .eth.Contract(abi, contractAddress);
@@ -27,5 +43,19 @@ export const filterCampaignInstance = async (web3, abi, contractAddress, userAcc
     donate: await campaignMethods.donate,
     refundDonation: await campaignMethods.refundDonation,
     claim: await campaignMethods.claim,
+  };
+};
+
+export const filterNftInstance = async (web3, abi, contractAddress) => {
+  const { methods: nftRewardMethods } = new web3
+    .eth.Contract(abi, contractAddress);
+
+  const { name, image, description } = await
+  fetch(await nftRewardMethods.tokenURI(0).call()).then((res) => res.json());
+
+  return {
+    name,
+    image: image.replace('ipfs://', 'https://ipfs.io/ipfs/'),
+    description,
   };
 };
